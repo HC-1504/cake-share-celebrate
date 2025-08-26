10b0737137197bf009640578c10b411327485cfa
Tharani1312 <tharanir-jm23@student.tarc.edu.my>
2025-08-26 14:30:40 +0800

    Update CakeCheckin.sol

--- contracts/CakeCheckin.sol
+++ contracts/CakeCheckin.sol
@@ -2,11 +2,7 @@
 pragma solidity ^0.8.19;
 
 contract CakeCheckin {
-    enum Status {
-        None,
-        In,
-        Out
-    }
+    enum Status { None, In, Out }
 
     struct Attendance {
         Status status;
@@ -14,42 +10,28 @@ contract CakeCheckin {
         uint64 checkOutAt;
     }
 
-    // Store attendance for each user
     mapping(address => Attendance) private _attendance;
 
-    // Events
     event CheckedIn(address indexed user, uint64 timestamp);
     event CheckedOut(address indexed user, uint64 timestamp);
 
-    /// @notice Check in to the event
     function checkIn() external {
-        Attendance storage a = _attendance[msg.sender];
-        require(a.status == Status.None, "Already checked in or out");
-
-        a.status = Status.In;
-        a.checkInAt = uint64(block.timestamp);
-
-        emit CheckedIn(msg.sender, a.checkInAt);
+        Attendance storage att = _attendance[msg.sender];
+        require(att.status == Status.None, "Already checked in/out");
+        att.status = Status.In;
+        att.checkInAt = uint64(block.timestamp);
+        emit CheckedIn(msg.sender, att.checkInAt);
     }
 
-    /// @notice Check out of the event
     function checkOut() external {
-        Attendance storage a = _attendance[msg.sender];
-        require(a.status == Status.In, "Not checked in or already checked out");
-
-        a.status = Status.Out;
-        a.checkOutAt = uint64(block.timestamp);
-
-        emit CheckedOut(msg.sender, a.checkOutAt);
+        Attendance storage att = _attendance[msg.sender];
+        require(att.status == Status.In, "Not checked in");
+        att.status = Status.Out;
+        att.checkOutAt = uint64(block.timestamp);
+        emit CheckedOut(msg.sender, att.checkOutAt);
     }
 
-    /// @notice Get a user’s attendance info
-    function getAttendance(address user)
-        external
-        view
-        returns (Status status, uint64 checkInAt, uint64 checkOutAt)
-    {
-        Attendance storage a = _attendance[user];
-        return (a.status, a.checkInAt, a.checkOutAt);
+    function getStatus(address user) external view returns (Status) {
+        return _attendance[user].status;
     }
 }
